9a355604c567ea7125fb734f5c6f475fb5ae70b5
Pranav <pranavpk0@outlook.com>
2021-10-11 21:51:00 +0530

    Added geolocation and reverse geolocation

--- index.js
+++ index.js
@@ -1,101 +1,65 @@
 const apikey = "3265874a2c77ae4a04bb96236a642d2f";
-
 const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
-
-// TYPE
-const url = (city) =>
-  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
-
-async function getWeatherByLocation(city) {
-  const resp = await fetch(url(city), { origin: "cors" });
-  const respData = await resp.json();
-
-  console.log(respData);
-
-  addWeatherToPage(respData);
+function KtoC(K) {
+  return Math.floor(K - 273.15);
 }
-
 function addWeatherToPage(data) {
   const temp = KtoC(data.main.temp);
-
   const weather = document.createElement("div");
   weather.classList.add("weather");
-
   weather.innerHTML = `
         <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
-        <small>${data.weather[0].main}</small>
-    `;
+        <small>${data.weather[0].main}</small>`;
 
-  // cleanup
   main.innerHTML = "";
-
   main.appendChild(weather);
 }
 
-function KtoC(K) {
-  return Math.floor(K - 273.15);
+// Manual
+const url = (city) =>
+  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+
+async function getWeatherByName(city) {
+  const resp = await fetch(url(city), { origin: "cors" });
+  const respData = await resp.json();
+  addWeatherToPage(respData);
 }
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const city = search.value;
-
+  ``;
   if (city) {
-    getWeatherByLocation(city);
+    getWeatherByName(city);
   }
 });
-
+//
 // Geolocation
 function success(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
-  console.log("latitude " + latitude + " longitude " + longitude);
-  var url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`;
+  let urlL = `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`;
 
   async function getWeatherByLocation() {
     const resp = await fetch(url, { origin: "cors" });
     const respData = await resp.json();
 
+    const respLocation = await fetch(urlL);
+    const respDataLocation = await respLocation.json();
     console.log(respData);
-
     addWeatherToPage(respData);
+    addLocation(respDataLocation);
   }
-
-  function addWeatherToPage(data) {
-    const temp = KtoC(data.main.temp);
-
-    const weather = document.createElement("div");
-    weather.classList.add("weather");
-
-    weather.innerHTML = `
-          <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
-          <small>${data.weather[0].main}</small>
-      `;
-    // form.innerText = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-    // cleanup
-    main.innerHTML = "";
-
-    main.appendChild(weather);
-  }
-  function KtoC(K) {
-    return Math.floor(K - 273.15);
-  }
-
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const city = search.value;
-
-    if (city) {
-    }
-  });
-
   getWeatherByLocation();
 }
-
+function addLocation(location) {
+  document.getElementById("location").innerText = location.display_name;
+}
+// function add
 function error() {
   console.log("Unable to retrieve your location");
 }
