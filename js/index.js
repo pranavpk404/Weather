12e6a3b4476f62875bbcc1f65106154d2366982b
Pranav <pranavpk0@outlook.com>
2021-10-13 20:15:31 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,15 +2,125 @@ const apikey = "3265874a2c77ae4a04bb96236a642d2f";
 const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
-// https://api.openweathermap.org/data/2.5/forecast?q=kannur&appid=3265874a2c77ae4a04bb96236a642d2f
 function KtoC(K) {
   return Math.floor(K - 273.15);
 }
-async function forecast() {
-  const url = `https://api.openweathermap.org/data/2.5/forecast?q=kannur&appid=${apikey}`;
-  const forecastD = await fetch(url, { origin: "cors" });
-  const forecastJ = await forecastD.json();
-  processforecastJ(forecastJ.list);
+
+// forecast();
+
+function addWeatherToPage(data, location) {
+  // Date
+  let d = new Date();
+  let date = d.getDate();
+  let month = d.getMonth() + 1;
+  let year = d.getFullYear();
+
+  const temp = KtoC(data.main.temp);
+  const weather = document.createElement("div");
+  weather.classList.add("weather");
+  weather.innerHTML = `
+    <div class="card" id="card">
+      <h2>${date}/${month}/${year}</h2>
+      <h2 id="location"></h2>
+      <h3>${data.weather[0].main}</h3>
+      <h1>${temp}°C</h1>
+      <div class="img"><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></div>
+        <table id="table">
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </table>
+  </div>
+
+`;
+  main.innerHTML = "";
+  main.appendChild(weather);
+  forecast(location);
+}
+
+// Manual
+try {
+  async function getWeatherByName(city) {
+    const url = (city) =>
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+    var transfer = city;
+
+    const resp = await fetch(url(city), { origin: "cors" });
+    const respData = await resp.json();
+    addWeatherToPage(respData);
+    addLocationM(respData);
+  }
+  function addLocationM(city) {
+    city = city.name;
+    document.getElementById("location").innerText = city;
+  }
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const city = search.value;
+    ``;
+    if (city) {
+      getWeatherByName(city, city);
+    }
+  });
+} catch {
+  let error = document.getElementById("error");
+  error.innerText = "Some Error Have Occurred";
+}
+
+// Geolocation
+function success(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`;
+  let urlL = `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`;
+
+  async function getWeatherByLocation() {
+    const resp = await fetch(url, { origin: "cors" });
+    const respData = await resp.json();
+
+    const respLocation = await fetch(urlL, { origin: "cors" });
+    const respDataLocation = await respLocation.json();
+
+    addWeatherToPage(respData, respDataLocation.address.county);
+    addLocation(respDataLocation);
+  }
+  getWeatherByLocation();
+}
+function addLocation(location) {
+  document.getElementById("location").innerText = location.address.county;
+  forecast(location.address.county);
+}
+
+function error() {
+  console.log("Unable to retrieve your location");
+}
+navigator.geolocation.getCurrentPosition(success, error);
+
+async function forecast(city) {
+  try {
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}`;
+    const forecastD = await fetch(url, { origin: "cors" });
+    const forecastJ = await forecastD.json();
+    processforecastJ(forecastJ.list);
+  } catch (error) {
+    let city = search.value;
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}`;
+    const forecastD = await fetch(url, { origin: "cors" });
+    const forecastJ = await forecastD.json();
+    processforecastJ(forecastJ.list);
+  }
 }
 
 function processforecastJ(list) {
@@ -49,96 +159,3 @@ function processforecastJ(list) {
 
   t.innerHTML = n;
 }
-
-// forecast();
-
-function addWeatherToPage(data) {
-  // Date
-  let d = new Date();
-  let date = d.getDate();
-  let month = d.getMonth() + 1;
-  let year = d.getFullYear();
-
-  const temp = KtoC(data.main.temp);
-  const weather = document.createElement("div");
-  weather.classList.add("weather");
-  weather.innerHTML = `
-    <div class="card" id="card">
-      <h2>${date}/${month}/${year}</h2>
-      <h2 id="location"></h2>
-      <h3>${data.weather[0].main}</h3>
-      <h1>${temp}°C</h1>
-      <div class="img"><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></div>
-        <table id="table">
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-      <td></td>
-    </tr>
-  </table>
-  </div>
-
-`;
-  main.innerHTML = "";
-  main.appendChild(weather);
-  forecast();
-}
-
-// Manual
-
-async function getWeatherByName(city) {
-  const url = (city) =>
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
-  const resp = await fetch(url(city), { origin: "cors" });
-  const respData = await resp.json();
-  addWeatherToPage(respData);
-  addLocationM(respData);
-}
-function addLocationM(city) {
-  city = city.name;
-  document.getElementById("location").innerText = city;
-}
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const city = search.value;
-  ``;
-  if (city) {
-    getWeatherByName(city);
-  }
-});
-
-// Geolocation
-function success(position) {
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`;
-  let urlL = `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`;
-  async function getWeatherByLocation() {
-    const resp = await fetch(url, { origin: "cors" });
-    const respData = await resp.json();
-
-    const respLocation = await fetch(urlL, { origin: "cors" });
-    const respDataLocation = await respLocation.json();
-    addWeatherToPage(respData);
-    addLocation(respDataLocation);
-  }
-  getWeatherByLocation();
-}
-function addLocation(location) {
-  document.getElementById("location").innerText = location.address.county;
-}
-
-function error() {
-  console.log("Unable to retrieve your location");
-}
-navigator.geolocation.getCurrentPosition(success, error);
