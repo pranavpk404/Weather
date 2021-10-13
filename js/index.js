563387ce2ce3a829c094f51e67c1471979918eb9
Pranav <pranavpk0@outlook.com>
2021-10-13 16:58:21 +0530

    All Done

---
+++ js/index.js
@@ -0,0 +1,145 @@
+const apikey = "3265874a2c77ae4a04bb96236a642d2f";
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+// https://api.openweathermap.org/data/2.5/forecast?q=kannur&appid=3265874a2c77ae4a04bb96236a642d2f
+function KtoC(K) {
+  return Math.floor(K - 273.15);
+}
+async function forecast() {
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=kannur&appid=${apikey}`;
+  const forecastD = await fetch(url, { origin: "cors" });
+  const forecastJ = await forecastD.json();
+  processforecastJ(forecastJ.list);
+}
+
+function processforecastJ(list) {
+  let day1 = list.slice(9, 10);
+  let day2 = list.slice(17, 18);
+  let day3 = list.slice(25, 26);
+  let day4 = list.slice(33, 34);
+  let day5 = list.slice(39, 40);
+
+  let day1D = day1[0].dt_txt.split(" ");
+  let day2D = day2[0].dt_txt.split(" ");
+  let day3D = day3[0].dt_txt.split(" ");
+  let day4D = day4[0].dt_txt.split(" ");
+  let day5D = day5[0].dt_txt.split(" ");
+
+  let n = `
+  <table id="table">
+      <tr>
+        <td>${day1D[0]}</td>
+        <td>${day2D[0]}</td>
+        <td>${day3D[0]}</td>
+        <td>${day4D[0]}</td>
+        <td>${day5D[0]}</td>
+      </tr>
+      <tr>
+        <td>${KtoC(day1[0].main.temp)}°</td>
+        <td>${KtoC(day2[0].main.temp)}°</td>
+        <td>${KtoC(day3[0].main.temp)}°</td>
+        <td>${KtoC(day4[0].main.temp)}°</td>
+        <td>${KtoC(day5[0].main.temp)}°</td>
+      </tr>
+
+
+    </table>`;
+  let t = document.getElementById("table");
+
+  t.innerHTML = n;
+}
+
+// forecast();
+
+function addWeatherToPage(data) {
+  // Date
+  let d = new Date();
+  let date = d.getDate();
+  let month = d.getMonth() + 1;
+  let year = d.getFullYear();
+
+  const temp = KtoC(data.main.temp);
+  const weather = document.createElement("div");
+  weather.classList.add("weather");
+  weather.innerHTML = `
+    <div class="card" id="card">
+      <h2>${date}/${month}/${year}</h2>
+      <h2 id="location"></h2>
+      <h3>${data.weather[0].main}</h3>
+      <h1>${temp}°C</h1>
+      <div class="img"><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></div>
+        <table id="table">
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </table>
+  </div>
+
+`;
+  main.innerHTML = "";
+  main.appendChild(weather);
+  forecast();
+}
+
+// Manual
+
+async function getWeatherByName(city) {
+  const url = (city) =>
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+  const resp = await fetch(url(city), { origin: "cors" });
+  const respData = await resp.json();
+  addWeatherToPage(respData);
+  addLocationM(respData);
+}
+function addLocationM(city) {
+  city = city.name;
+  document.getElementById("location").innerText = city;
+}
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const city = search.value;
+  ``;
+  if (city) {
+    getWeatherByName(city);
+  }
+});
+
+// Geolocation
+function success(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`;
+  let urlL = `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`;
+  async function getWeatherByLocation() {
+    const resp = await fetch(url, { origin: "cors" });
+    const respData = await resp.json();
+
+    const respLocation = await fetch(urlL, { origin: "cors" });
+    const respDataLocation = await respLocation.json();
+    addWeatherToPage(respData);
+    addLocation(respDataLocation);
+  }
+  getWeatherByLocation();
+}
+function addLocation(location) {
+  location = `${location.address.neighbourhood}, ${location.address.suburb}, ${location.address.county}`;
+  document.getElementById("location").innerText = location;
+}
+
+function error() {
+  console.log("Unable to retrieve your location");
+}
+navigator.geolocation.getCurrentPosition(success, error);
